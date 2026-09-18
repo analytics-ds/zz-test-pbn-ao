476b6939d7c6ae643ba6fae48fc69a6ddcfe149a
La redaction <redaction@users.noreply.github.com>
2026-09-18 14:51:10 +0200

    Le depot ne porte que les pages publiees

--- build_all.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Régénère tout le site : accueil, catégories, articles, pages fixes, sitemap/robots/llms."""
-import subprocess, sys, os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-for s in ("build_home.py", "build_categories.py", "build_articles.py", "build_pages.py", "build_sitemap.py"):
-    subprocess.run([sys.executable, s], check=True)
